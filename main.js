d92ee76b6fcf02932faeb2c9681ef7c95d7f8212
Maria Clara ☆ <104856731+MariaClara-Oderdenge@users.noreply.github.com>
2024-03-15 10:51:12 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -14,5 +13,11 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 
